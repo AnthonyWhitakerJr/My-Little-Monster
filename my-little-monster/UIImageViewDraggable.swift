9e3ce4e9ee653edc12e167cf94d7a7371b968195
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-07-15 15:51:06 -0400

    Relabel skulls for debugging. Send notification when care item is dropped in monster. Observe notification of item drop. Add logic for lives.

--- my-little-monster/UIImageViewDraggable.swift
+++ my-little-monster/UIImageViewDraggable.swift
@@ -12,6 +12,7 @@ import UIKit
 class UIImageViewDraggable : UIImageView {
     
     var originalPosition: CGPoint!
+    var dropTarget: UIView?
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         originalPosition = self.center
@@ -24,6 +25,13 @@ class UIImageViewDraggable : UIImageView {
     }
 
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first, let target = dropTarget {
+            let position = touch.locationInView(self.superview)
+            if CGRectContainsPoint(target.frame, position){
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
+            }
+        }
+        
         self.center = originalPosition
     }
 }
--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -15,11 +15,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var foodImage: UIImageViewDraggable!
     @IBOutlet weak var monsterImage: UIImageViewMonster!
     
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    
+    var maxPenalties : Int {
+        return skulls.count
+    }
+    
+    var penalties = 0
+    var timer: NSTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        heartImage.dropTarget = monsterImage
+        foodImage.dropTarget = monsterImage
         
+        for skull in skulls {
+            skull.alpha = DIM_ALPHA
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
+        
+        startTimer()
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("ITEM DROPPED")
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        for skull in skulls {
+            skull.alpha = DIM_ALPHA
+        }
+        
+        penalties += 1
+        
+        for i in 0...penalties - 1 {
+            skulls[i].alpha = OPAQUE
+        }
+        
+        if penalties >= maxPenalties {
+            gameOver()
+        }
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImage.playDeathAnimation()
     }
 
 }
